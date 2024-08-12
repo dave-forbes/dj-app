2048230d01f96c42a6be68c9e2f6ddbf2bf7fc29
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-08-12 21:09:09 +0100

    refactor setupAudioControls
    - swap unnecessary parameters for just deckNo, to reduce repetition
    - import and run initalize eq

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import './styles.css';
 import './input-knobs.js';
+import initializeEQ from './eq.js';
 
-function setupAudioControls(
-  uploadId,
-  audioId,
-  speedControlId,
-  textId,
-  volumeControlId,
-  armId,
-  stopStartId,
-  deckNo
-) {
-  const audioUpload = document.getElementById(uploadId);
-  const audioElement = document.getElementById(audioId);
-  const speedControl = document.getElementById(speedControlId);
-  const volumeControl = document.getElementById(volumeControlId);
-  const arm = document.getElementById(armId);
-  const playPauseButton = document.getElementById(stopStartId);
+function setupAudioControls(deckNo) {
+  const audioUpload = document.getElementById(
+    `audioUpload-${deckNo}`
+  );
+  const audioElement = document.getElementById(
+    `audioElement-${deckNo}`
+  );
+  const speedControl = document.getElementById(
+    `speedControl-${deckNo}`
+  );
+  const trackLabel = document.getElementById(`text-${deckNo}`);
+  const volumeControl = document.getElementById(
+    `volumeControl-${deckNo}`
+  );
+  const arm = document.getElementById(`arm-${deckNo}`);
+  const playPauseButton = document.getElementById(
+    `stop-start-${deckNo}`
+  );
 
   audioElement.volume = volumeControl.value;
 
@@ -31,8 +34,7 @@ function setupAudioControls(
       const url = URL.createObjectURL(file);
       audioElement.src = url;
       audioElement.load();
-      const text = document.querySelector(textId);
-      text.textContent = file.name;
+      trackLabel.textContent = file.name;
       arm.style.cssText = `transform:rotate(0deg )`;
       if (arm.classList.contains('play'))
         arm.classList.remove('play');
@@ -81,28 +83,12 @@ function setupAudioControls(
       playPauseButton.textContent = 'Play';
     }
   });
+
+  initializeEQ(deckNo);
 }
 
 // Setup controls for the first audio element
-setupAudioControls(
-  'audioUpload-1',
-  'audioElement-1',
-  'speedControl-1',
-  '#text-1',
-  'volumeControl-1',
-  'arm-1',
-  'stop-start-1',
-  1
-);
+setupAudioControls(1);
 
 // Setup controls for the second audio element
-setupAudioControls(
-  'audioUpload-2',
-  'audioElement-2',
-  'speedControl-2',
-  '#text-2',
-  'volumeControl-2',
-  'arm-2',
-  'stop-start-2',
-  2
-);
+setupAudioControls(2);
