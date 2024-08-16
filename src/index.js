9ef38de725287e8064d24c35e45a270a4c5ddcbe
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-08-16 13:12:54 +0100

    refactored
    - separated logic out into their own files and imported back in to improve readability

--- src/index.js
+++ src/index.js
@@ -1,89 +1,53 @@
 import './styles.css';
-import './input-knobs.js';
-import initializeEQ from './eq.js';
+import './js/input-knobs.js';
+import getDomElements from './js/getDomElements';
+import createAudioContext from './js/createAudioContext';
+import createEqFilters from './js/createEQFilters';
+import connectFilters from './js/connectFilters';
+import setupEventListeners from './js/setupEventListeners';
 
-  const audioUpload = document.getElementById(
-    `audioUpload-${deckNo}`
+function initilizedecks(deckNo) {
+  const elements = getDomElements(deckNo);
+  const { audioElement, audioContext, source } = createAudioContext(
+    deckNo,
+    elements.volumeControl
   );
-  const audioElement = document.getElementById(
-    `audioElement-${deckNo}`
-  );
-  const speedControl = document.getElementById(
-    `speedControl-${deckNo}`
-  );
-  const trackLabel = document.getElementById(`text-${deckNo}`);
-  const volumeControl = document.getElementById(
-    `volumeControl-${deckNo}`
-  );
-  const arm = document.getElementById(`arm-${deckNo}`);
-  const playPauseButton = document.getElementById(
-    `stop-start-${deckNo}`
-  );
+  const filters = createEqFilters(audioContext);
 
-  audioElement.volume = volumeControl.value;
-
-  volumeControl.addEventListener('input', function () {
-    audioElement.volume = this.value;
-  });
-
-  audioUpload.addEventListener('change', function () {
-    const files = this.files;
-    if (files.length > 0) {
-      const file = files[0];
-      const url = URL.createObjectURL(file);
-      audioElement.src = url;
-      audioElement.load();
-      trackLabel.textContent = file.name;
-      arm.style.cssText = `transform:rotate(0deg )`;
-      if (arm.classList.contains('play'))
-        arm.classList.remove('play');
-      playPauseButton.textContent = 'Play';
-    }
-  });
-
-  speedControl.addEventListener('input', function () {
-    audioElement.playbackRate = this.value;
-  });
+  connectFilters(
+    source,
+    filters.bassFilter,
+    filters.midFilter,
+    filters.trebleFilter,
+    audioContext
+  );
+  setupEventListeners(
+    elements,
+    audioElement,
+    audioContext,
+    filters,
+    deckNo
+  );
 
   audioElement.preservesPitch = false;
 
   audioElement.addEventListener('timeupdate', () => {
     const totalDuration = audioElement.duration;
     const currentTime = audioElement.currentTime;
-    // Check if duration is available
     if (totalDuration > 0) {
-      // Calculate the percentage of the current progress
-      const progressPercentage = (currentTime / totalDuration) * 18;
-      arm.style.cssText = `transform:rotate(${
-        20 + progressPercentage
+      const progressPercentage = (currentTime / totalDuration) * 100;
+      elements.arm.style.cssText = `transform:rotate(${
+        20 + (currentTime / totalDuration) * 18
       }deg )`;
       const record = document.querySelector(
         `.deck-${deckNo} .record`
       );
       record.style.cssText = `transform:rotate(${
-        progressPercentage * 360
+        (currentTime / totalDuration) * 360
       }deg)`;
+      elements.seekControl.value = progressPercentage;
     }
   });
-
-  playPauseButton.addEventListener('click', () => {
-    const arm = document.querySelector(`#arm-${deckNo}`);
-    const audioElement = document.querySelector(
-      `#audioElement-${deckNo}`
-    );
-    const record = document.querySelector(`.deck-${deckNo} .record`);
-    arm.classList.toggle('play');
-    record.classList.toggle('play');
-    if (arm.classList.contains('play')) {
-      audioElement.play();
-      playPauseButton.textContent = 'Stop';
-    } else {
-      audioElement.pause();
-      playPauseButton.textContent = 'Play';
-    }
-  });
-
-  initializeEQ(deckNo);
 }
 
 // Setup controls for the first audio element
